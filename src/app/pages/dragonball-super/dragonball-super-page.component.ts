96266d7ec710ff718d088c38f6e3bfc6bf964fc1
JaviSrz <ljsuarez7@gmail.com>
2025-03-26 19:59:03 +0100

    creado servicio para centralizar la logica

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,13 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
-import { Character } from '../../interfaces/character.interface';
-
-// interface Character {
-//   id: number;
-//   name: string;
-//   power: number;
-// }
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   selector: 'dragonball-super',
@@ -16,15 +10,6 @@ import { Character } from '../../interfaces/character.interface';
 })
 export class DragonballSuperPageComponent {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001},
-    { id: 2, name: 'Vegeta', power: 8000},
-  ]);
-
-  addCharacter(character: Character){
-    this.characters.update(
-      list => [...list, character]
-    );
-  }
+  public dragonballService = inject(DragonballService);
 
 }
