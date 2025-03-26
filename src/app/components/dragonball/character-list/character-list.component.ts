fa6248b9d67f7bd25ac4b525b36fcfb42c2bb1db
JaviSrz <ljsuarez7@gmail.com>
2025-03-26 19:31:58 +0100

    añadido titulo como input al listado y creado componente para el formulario de creación de personaje

--- src/app/components/dragonball/character-list/character-list.component.ts
+++ src/app/components/dragonball/character-list/character-list.component.ts
@@ -7,6 +7,7 @@ import type { Character } from '../../../interfaces/character.interface';
 })
 export class CharacterListComponent {
 
-  characters = input.required<Character[]>()
+  characters = input.required<Character[]>();
+  listName = input.required<string>();
 
 }
